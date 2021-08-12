ca62cabda4a6c27f5ac9b0e5173e3483f14a0cec
Danny Mussie <dannymussie7@gmail.com>
2021-08-12 16:03:54 -0500

    created addToCollection function and added album object to push into collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,11 @@
 console.log('***** Music Collection *****')
 let collection = [];
+function addToCollection (title, artist, yearPublished) {
+  let album = {
+    title:
+    artist:
+    yearPublished:
+  } // end album
+  collection.push(album);
+  return album;
+} // end addToCollection
